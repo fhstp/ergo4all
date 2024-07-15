b8bb9fd8e840e57260e55b737cf27e759d604b5f
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 10:11:16 +0200

    add placeholder tos text

--- lib/screens/terms_of_use.dart
+++ lib/screens/terms_of_use.dart
@@ -9,6 +9,15 @@ class TermsOfUseScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Terms of use"),
       ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+          ],
+        ),
+      ),
     );
   }
 }
