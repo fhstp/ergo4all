9cf9c5082477b9ad9bde87419061cfbbb4c8ffa5
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 10:18:09 +0200

    add accept checkbox

--- lib/screens/terms_of_use.dart
+++ lib/screens/terms_of_use.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
-class TermsOfUseScreen extends StatelessWidget {
+const tosText =
+    "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
+class TermsOfUseScreen extends StatefulWidget {
   const TermsOfUseScreen({super.key});
+
+  @override
+  State<TermsOfUseScreen> createState() => _TermsOfUseScreenState();
+}
+
+class _TermsOfUseScreenState extends State<TermsOfUseScreen> {
+  bool hasAccepted = false;
+
+  void toggleHasAccepted() {
+    setState(() {
+      hasAccepted = !hasAccepted;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,12 +25,21 @@ class TermsOfUseScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Terms of use"),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-                "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+            const Text(tosText),
+            Row(
+              children: [
+                const Text("I accept"),
+                Checkbox(
+                    value: hasAccepted,
+                    onChanged: (isChecked) {
+                      toggleHasAccepted();
+                    }),
+              ],
+            )
           ],
         ),
       ),
