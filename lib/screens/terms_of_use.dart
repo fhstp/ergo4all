9e090fa4447d1064117c508536116b7241e72575
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 10:09:28 +0200

    add app bar title

--- lib/screens/terms_of_use.dart
+++ lib/screens/terms_of_use.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TermsOfUseScreen extends StatelessWidget {
   const TermsOfUseScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Terms of use"),
+      ),
+    );
   }
 }
