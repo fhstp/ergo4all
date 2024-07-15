cbefee46786cf27237bdf555a5de299646078209
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 10:51:06 +0200

    add appbar

--- lib/screens/pre_user_creator.dart
+++ lib/screens/pre_user_creator.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PreUserCreatorScreen extends StatelessWidget {
   const PreUserCreatorScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Creating user"),
+      ),
+    );
   }
 }
