242e8ef610a63eb4471222235239458b13a594fc
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 16:33:59 +0200

    add navigation after delay

--- lib/screens/analysis.dart
+++ lib/screens/analysis.dart
@@ -1,9 +1,32 @@
+import 'package:ergo4all/screens/results.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/scheduler.dart';
 import '../widgets/loading_indicator.dart';
 
-class AnalysisScreen extends StatelessWidget {
+class AnalysisScreen extends StatefulWidget {
   const AnalysisScreen({super.key});
+
+  @override
+  State<AnalysisScreen> createState() => _AnalysisScreenState();
+}
+
+class _AnalysisScreenState extends State<AnalysisScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) {
+      _navigateAfterDelay();
+    });
+  }
+
+  _navigateAfterDelay() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const ResultsScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
