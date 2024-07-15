ed70d61d6f05e262f325fbebc45984f02a3eb487
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 12:21:12 +0200

    feat: add skeleton home screen (#6)
    * add app bar
    
    * add bottom bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,24 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: const Placeholder(),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.upload), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.camera_alt), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: "")
+        ],
+      ),
+    );
   }
 }
