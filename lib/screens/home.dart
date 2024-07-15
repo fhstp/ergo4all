4fa293ec643a794ac5c1b7c31ecea5aec43e9e23
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 15:38:42 +0200

    navigate to analysis on upload

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,28 @@
+import 'package:ergo4all/io/video.dart';
+import 'package:ergo4all/screens/analysis.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final Future<XFile?> Function() tryGetVideo;
+
+  const HomeScreen({super.key, this.tryGetVideo = tryGetVideoFromGallery});
 
   @override
   Widget build(BuildContext context) {
+    final navigator = Navigator.of(context);
+
+    void analyzeVideo(XFile videoFile) {
+      navigator.push(MaterialPageRoute(builder: (_) => const AnalysisScreen()));
+    }
+
+    void trySelectVideoForAnalysis() async {
+      final videoFile = await tryGetVideo();
+      if (videoFile == null) return;
+
+      analyzeVideo(videoFile);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home"),
@@ -14,7 +32,10 @@ class HomeScreen extends StatelessWidget {
           child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.upload)),
+          IconButton(
+              key: const Key("upload"),
+              onPressed: trySelectVideoForAnalysis,
+              icon: const Icon(Icons.upload)),
           IconButton(onPressed: () {}, icon: const Icon(Icons.camera_alt)),
           IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
         ],
