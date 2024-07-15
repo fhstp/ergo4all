055d45edeee0228cfc1201daa63ee6c57e283e63
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 16:18:36 +0200

    add basic loading indicator

--- lib/screens/analysis.dart
+++ lib/screens/analysis.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AnalysisScreen extends StatelessWidget {
   const AnalysisScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Analyzing your ergonomics"),
+            FractionallySizedBox(
+              widthFactor: 0.5,
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Stack(children: [
+                  Center(
+                    child: FractionallySizedBox(
+                        widthFactor: 0.5,
+                        heightFactor: 0.5,
+                        child: Image(
+                            image:
+                                AssetImage('assets/images/logos/IconRed.png'))),
+                  ),
+                  SizedBox.expand(child: CircularProgressIndicator())
+                ]),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
