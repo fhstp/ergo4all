5d578ac68fce382193c4c8ff49c1118be72764cb
Ramon Brullo <kingvanti650@gmail.com>
2024-07-15 09:53:31 +0200

    feat: add skeleton pre-intro screen
    commit 1ef3d2d1d0274a0e67e79ab85ba9435acdec01bc
    Author: Ramon Brullo <kingvanti650@gmail.com>
    Date:   Mon Jul 15 09:53:03 2024 +0200
    
        add skip button
    
    commit ba7d3ebce48d5ecd15e22f8321d2166370d81b37
    Author: Ramon Brullo <kingvanti650@gmail.com>
    Date:   Mon Jul 15 09:46:13 2024 +0200
    
        add header
    
    commit 88826a560cbdaa249cc1c39f2ed29a5de3a53662
    Author: Ramon Brullo <kingvanti650@gmail.com>
    Date:   Mon Jul 15 09:45:51 2024 +0200
    
        add logo

--- lib/screens/pre_intro.dart
+++ lib/screens/pre_intro.dart
@@ -1,3 +1,4 @@
+import 'package:ergo4all/screens/terms_of_use.dart';
 import 'package:flutter/material.dart';
 
 class PreIntroScreen extends StatelessWidget {
@@ -5,8 +6,23 @@ class PreIntroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Placeholder(),
+    final navigator = Navigator.of(context);
+
+    void skipIntroduction() {
+      navigator
+          .push(MaterialPageRoute(builder: (_) => const TermsOfUseScreen()));
+    }
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Image(image: AssetImage('assets/images/logos/LogoRed.png')),
+          const Text("Choose your profile"),
+          GestureDetector(
+              onTap: skipIntroduction, child: const Text("Skip introduction"))
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/terms_of_use.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class TermsOfUseScreen extends StatelessWidget {
+  const TermsOfUseScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
