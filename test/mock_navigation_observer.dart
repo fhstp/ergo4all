145fbd6ac64ec1ce27ad1a1bd9dc86829719091b
Ramon Brullo <kingvanti650@gmail.com>
2024-07-22 12:01:32 +0200

    refactor: encapsulate fields
    Make fields in MockNavigationObserver private and final.

--- test/mock_navigation_observer.dart
+++ test/mock_navigation_observer.dart
@@ -3,31 +3,38 @@ import 'package:flutter/material.dart';
 typedef NavigateCallback = void Function(Route? route, Route? previousRoute);
 
 class MockNavigationObserver extends NavigatorObserver {
-  NavigateCallback? pushed;
-  NavigateCallback? popped;
-  NavigateCallback? removed;
-  NavigateCallback? replaced;
+  final NavigateCallback? _pushed;
+  final NavigateCallback? _popped;
+  final NavigateCallback? _removed;
+  final NavigateCallback? _replaced;
 
   MockNavigationObserver(
-      {this.pushed, this.popped, this.removed, this.replaced});
+      {NavigateCallback? pushed,
+      NavigateCallback? popped,
+      NavigateCallback? removed,
+      NavigateCallback? replaced})
+      : _replaced = replaced,
+        _removed = removed,
+        _popped = popped,
+        _pushed = pushed;
 
   @override
   void didPush(Route route, Route? previousRoute) {
-    pushed?.call(route, previousRoute);
+    _pushed?.call(route, previousRoute);
   }
 
   @override
   void didPop(Route route, Route? previousRoute) {
-    popped?.call(route, previousRoute);
+    _popped?.call(route, previousRoute);
   }
 
   @override
   void didRemove(Route route, Route? previousRoute) {
-    removed?.call(route, previousRoute);
+    _removed?.call(route, previousRoute);
   }
 
   @override
   void didReplace({Route? newRoute, Route? oldRoute}) {
-    replaced?.call(newRoute, oldRoute);
+    _replaced?.call(newRoute, oldRoute);
   }
 }
