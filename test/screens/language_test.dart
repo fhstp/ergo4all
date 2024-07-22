5f4eba479f3ecd81b4757d1326e0297d440b56ce
Ramon Brullo <kingvanti650@gmail.com>
2024-07-22 11:21:15 +0200

    allow setting providers in unit tests

--- test/screens/language_test.dart
+++ test/screens/language_test.dart
@@ -1,12 +1,18 @@
+import 'package:ergo4all/providers/custom_locale.dart';
 import 'package:ergo4all/screens/language.dart';
 import 'package:ergo4all/screens/pre_intro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 
 import '../mock_app.dart';
 import '../mock_navigation_observer.dart';
 
 void main() {
+  final mockCustomLocaleProvider = CustomLocale(() async {
+    return null;
+  }, (_) async {});
+
   testWidgets("should navigate to next screen once language button is pressed",
       (tester) async {
     bool didNavigate = false;
@@ -16,7 +22,9 @@ void main() {
     }
 
     await tester.pumpWidget(makeMockAppFromWidget(
-        const LanguageScreen(), MockNavigationObserver(pushed: onPushed)));
+        const LanguageScreen(),
+        MockNavigationObserver(pushed: onPushed),
+        [ChangeNotifierProvider(create: (_) => mockCustomLocaleProvider)]));
 
     await tester.tap(find.byKey(const Key("lang_button_deutsch")));
     await tester.pumpAndSettle();
