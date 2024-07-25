d454b91bab186e9a72c21b6135c3de30a3628074
Ramon Brullo <kingvanti650@gmail.com>
2024-07-25 15:16:58 +0200

    add basic professional intro screen

--- lib/screens/professional_intro.dart
+++ lib/screens/professional_intro.dart
@@ -1,4 +1,6 @@
+import 'package:ergo4all/widgets/screen_content.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ProfessionalIntro extends StatefulWidget {
   const ProfessionalIntro({super.key});
@@ -8,8 +10,63 @@ class ProfessionalIntro extends StatefulWidget {
 }
 
 class _ProfessionalIntroState extends State<ProfessionalIntro> {
+  int _pageIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _pageIndex);
+  }
+
+  void _onPageChanged(int newIndex) {
+    setState(() {
+      _pageIndex = newIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final appTheme = Theme.of(context);
+    final localizations = AppLocalizations.of(context)!;
+    final titles = [
+      localizations.professionalIntro_overview_title,
+      localizations.professionalIntro_privacy_title
+    ];
+
+    final skipButton = TextButton(onPressed: () {}, child: const Text("Skip"));
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titles[_pageIndex]),
+      ),
+      body: ScreenContent(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: _onPageChanged,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    localizations.professionalIntro_overview_header,
+                    style: appTheme.textTheme.headlineLarge,
+                  ),
+                  Text(localizations.professionalIntro_overview_text),
+                  skipButton
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    localizations.professionalIntro_privacy_header,
+                    style: appTheme.textTheme.headlineLarge,
+                  ),
+                  Text(localizations.professionalIntro_privacy_text),
+                  skipButton
+                ],
+              )
+            ],
+          )),
+    );
   }
 }
