a3dd72e7c8b2b4db976a86893e79b2701ea75b5d
Ramon Brullo <kingvanti650@gmail.com>
2024-07-25 14:34:49 +0200

    add navigation

---
+++ lib/screens/professional_intro.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfessionalIntro extends StatefulWidget {
+  const ProfessionalIntro({super.key});
+
+  @override
+  State<ProfessionalIntro> createState() => _ProfessionalIntroState();
+}
+
+class _ProfessionalIntroState extends State<ProfessionalIntro> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- test/screens/pre_intro_test.dart
+++ test/screens/pre_intro_test.dart
@@ -1,4 +1,5 @@
 import 'package:ergo4all/screens/pre_intro.dart';
+import 'package:ergo4all/screens/professional_intro.dart';
 import 'package:ergo4all/screens/terms_of_use.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,6 +8,22 @@ import '../mock_app.dart';
 import '../mock_navigation_observer.dart';
 
 void main() {
+  testWidgets("should navigate to professional intro when button is pressed",
+      (tester) async {
+    final mockNavigationObserver = MockNavigationObserver();
+
+    await tester.pumpWidget(makeMockAppFromWidget(
+      const PreIntroScreen(),
+      mockNavigationObserver,
+    ));
+
+    await tester.tap(find.byKey(const Key("professional")));
+    await tester.pumpAndSettle();
+
+    expect(mockNavigationObserver.anyNavigationHappened, isTrue);
+    expect(find.byType(ProfessionalIntro), findsOneWidget);
+  });
+
   testWidgets("should navigate to next screen once skip button is pressed",
       (tester) async {
     final mockNavigationObserver = MockNavigationObserver();
