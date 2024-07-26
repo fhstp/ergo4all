198422cca26fab38e15c312465f53dbcce2e3083
Ramon Brullo <kingvanti650@gmail.com>
2024-07-26 11:39:42 +0200

    extract widget

--- lib/screens/professional_intro.dart
+++ lib/screens/professional_intro.dart
@@ -1,86 +1,40 @@
-import 'package:ergo4all/screens/terms_of_use.dart';
-import 'package:ergo4all/widgets/screen_content.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ProfessionalIntro extends StatefulWidget {
+import 'intro.dart';
+
+class ProfessionalIntro extends StatelessWidget {
   const ProfessionalIntro({super.key});
-
-  @override
-  State<ProfessionalIntro> createState() => _ProfessionalIntroState();
-}
-
-class _ProfessionalIntroState extends State<ProfessionalIntro> {
-  int _pageIndex = 0;
-  late PageController _pageController;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(initialPage: _pageIndex);
-  }
-
-  void _onPageChanged(int newIndex) {
-    setState(() {
-      _pageIndex = newIndex;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     final appTheme = Theme.of(context);
     final localizations = AppLocalizations.of(context)!;
-    final navigator = Navigator.of(context);
 
-    final pageTitle = switch (_pageIndex) {
-      0 => localizations.professionalIntro_overview_title,
-      1 => localizations.professionalIntro_privacy_title,
-      _ => throw Error()
-    };
+    var overviewPage = IntroPage(
+        title: localizations.professionalIntro_overview_title,
+        widget: Column(
+          children: [
+            Text(
+              localizations.professionalIntro_overview_header,
+              style: appTheme.textTheme.headlineLarge,
+            ),
+            Text(localizations.professionalIntro_overview_text),
+          ],
+        ));
 
-    void navigateToTermsOfUse() {
-      navigator
-          .push(MaterialPageRoute(builder: (_) => const TermsOfUseScreen()));
-    }
+    var privacyPage = IntroPage(
+        title: localizations.professionalIntro_privacy_title,
+        widget: Column(
+          children: [
+            Text(
+              localizations.professionalIntro_privacy_header,
+              style: appTheme.textTheme.headlineLarge,
+            ),
+            Text(localizations.professionalIntro_privacy_text),
+          ],
+        ));
 
-    var overviewPage = Column(
-      children: [
-        Text(
-          localizations.professionalIntro_overview_header,
-          style: appTheme.textTheme.headlineLarge,
-        ),
-        Text(localizations.professionalIntro_overview_text),
-      ],
-    );
-
-    var privacyPage = Column(
-      children: [
-        Text(
-          localizations.professionalIntro_privacy_header,
-          style: appTheme.textTheme.headlineLarge,
-        ),
-        Text(localizations.professionalIntro_privacy_text),
-      ],
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(pageTitle),
-      ),
-      body: ScreenContent(
-          child: Column(children: [
-        Expanded(
-          child: PageView(
-            controller: _pageController,
-            onPageChanged: _onPageChanged,
-            children: [overviewPage, privacyPage],
-          ),
-        ),
-        ElevatedButton(
-            key: const Key("done"),
-            onPressed: navigateToTermsOfUse,
-            child: Text(localizations.professionalInto_done))
-      ])),
-    );
+    return Intro(pages: [overviewPage, privacyPage]);
   }
 }
--- test/screens/intro_test.dart
+++ test/screens/intro_test.dart
@@ -1,4 +1,4 @@
-import 'package:ergo4all/screens/professional_intro.dart';
+import 'package:ergo4all/screens/intro.dart';
 import 'package:ergo4all/screens/terms_of_use.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,12 +7,17 @@ import '../mock_app.dart';
 import '../mock_navigation_observer.dart';
 
 void main() {
+  final testPage = IntroPage(title: "Some title", widget: const Placeholder());
+
   testWidgets("should navigate to tos once done button is pressed",
       (tester) async {
     final mockNavigationObserver = MockNavigationObserver();
 
     await tester.pumpWidget(makeMockAppFromWidget(
-        const ProfessionalIntro(), mockNavigationObserver));
+        Intro(
+          pages: [testPage],
+        ),
+        mockNavigationObserver));
 
     await tester.tap(find.byKey(const Key("done")));
     await tester.pumpAndSettle();
