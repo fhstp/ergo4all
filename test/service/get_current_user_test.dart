870e1cc76abec591e67ee643826687802d561f22
Ramon Brullo <kingvanti650@gmail.com>
2024-08-12 13:36:24 +0200

    add get current user logic

---
+++ test/service/get_current_user_test.dart
@@ -0,0 +1,33 @@
+import 'package:ergo4all/domain/user.dart';
+import 'package:ergo4all/io/user_config.dart';
+import 'package:ergo4all/service/get_current_user.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("should be null if there is no config", () async {
+    final getCurrentUser = makeGetCurrentUserFromConfig(() async => null);
+
+    final currentUser = await getCurrentUser();
+
+    expect(currentUser, isNull);
+  });
+
+  test("should be null if there config has no current user", () async {
+    final getCurrentUser = makeGetCurrentUserFromConfig(
+        () async => UserConfig(currentUserIndex: null, userEntries: []));
+
+    final currentUser = await getCurrentUser();
+
+    expect(currentUser, isNull);
+  });
+
+  test("should be user if config has current user", () async {
+    const userName = "John";
+    final getCurrentUser = makeGetCurrentUserFromConfig(() async => UserConfig(
+        currentUserIndex: 0, userEntries: [UserConfigEntry(name: userName)]));
+
+    final currentUser = await getCurrentUser();
+
+    expect(currentUser, equals(const User(name: userName)));
+  });
+}
