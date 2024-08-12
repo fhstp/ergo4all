6a3bc1b2938ce9b1980b9ad767dfe034f3e17d5d
Ramon Brullo <kingvanti650@gmail.com>
2024-08-12 15:41:07 +0200

    add default user

--- test/screens/pre_user_creation_test.dart
+++ test/screens/pre_user_creation_test.dart
@@ -1,18 +1,27 @@
+import 'dart:async';
+
+import 'package:ergo4all/domain/user.dart';
 import 'package:ergo4all/screens/home.dart';
 import 'package:ergo4all/screens/pre_user_creator.dart';
 import 'package:ergo4all/screens/user_creator.dart';
+import 'package:ergo4all/service/add_user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 
 import '../mock_app.dart';
 import '../mock_navigation_observer.dart';
 
 void main() {
+  tearDown(() async {
+    await GetIt.instance.reset();
+  });
+
   testWidgets(
       "should navigate to next screen once \"default values\" is selected",
       (tester) async {
     final mockNavigationObserver = MockNavigationObserver();
-
+    GetIt.instance.registerSingleton<AddUser>((_) async => null);
     await tester.pumpWidget(makeMockAppFromWidget(
         const PreUserCreatorScreen(), mockNavigationObserver));
 
@@ -23,10 +32,26 @@ void main() {
     expect(find.byType(HomeScreen), findsOneWidget);
   });
 
+  testWidgets("should create default user when \"default values\" is selected",
+      (tester) async {
+    final completer = Completer();
+    GetIt.instance.registerSingleton<AddUser>((user) async {
+      expect(user, equals(const User(name: "Ergo-fan")));
+      completer.complete();
+    });
+    await tester
+        .pumpWidget(makeMockAppFromWidget(const PreUserCreatorScreen()));
+
+    await tester.tap(find.byKey(const Key("default-values")));
+    await tester.pumpAndSettle();
+
+    expect(completer.isCompleted, isTrue);
+  });
+
   testWidgets("should navigate to user creator once create button is pressed",
       (tester) async {
     final mockNavigationObserver = MockNavigationObserver();
-
+    GetIt.instance.registerSingleton<AddUser>((_) async => null);
     await tester.pumpWidget(makeMockAppFromWidget(
         const PreUserCreatorScreen(), mockNavigationObserver));
 
