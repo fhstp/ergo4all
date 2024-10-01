0d3cd3220a76e200788270614f160a7654c4fa97
Ramon Brullo <kingvanti650@gmail.com>
2024-10-01 13:07:03 +0200

    feat: add icon to app bar
    This resolves #29.

--- lib/ui/app_bar.dart
+++ lib/ui/app_bar.dart
@@ -1,9 +1,18 @@
+import 'package:ergo4all/ui/spacing.dart';
 import 'package:flutter/material.dart';
 
 /// Makes an [AppBar] widget with the custom styles for this app.
-/// This inculdes having the given [title].
-AppBar makeCustomAppBar({required String title}) {
+/// This inculdes having the given [title]. It also has the Ergo4All icon in it
+/// unless disabled by setting [withIcon] to false.
+AppBar makeCustomAppBar({required String title, bool withIcon = true}) {
   return AppBar(
+    leading: withIcon
+        ? const Padding(
+            padding: EdgeInsets.all(smallSpace),
+            child:
+                Image(image: AssetImage('assets/images/logos/IconWhite.png')),
+          )
+        : null,
     title: Text(title),
     centerTitle: true,
   );
