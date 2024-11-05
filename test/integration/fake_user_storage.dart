7775c6677e23d9dc3aba958766a7ee14674ead94
Ramon Brullo <kingvanti650@gmail.com>
2024-11-05 08:17:50 +0100

    fix: fake user storage error
    List usage failed because:
    - the list was not growable
    - the _currentUserIndex was calculated incorrectly.
    
    Fixed both

--- test/integration/fake_user_storage.dart
+++ test/integration/fake_user_storage.dart
@@ -1,15 +1,15 @@
-import 'package:common/list_ext.dart';
 import 'package:common/types.dart';
 import 'package:user_management/src/types.dart';
 
 class FakeUserStorage implements UserStorage {
   int? _currentUserIndex;
-  List<User> _users = List.empty();
+  final List<User> _users = List.empty(growable: true);
 
   @override
   Future<Null> addUser(User user) async {
     _users.add(user);
-    _currentUserIndex = _users.length;
+    _currentUserIndex = _users.length - 1;
+    return null;
   }
 
   @override
@@ -24,6 +24,7 @@ class FakeUserStorage implements UserStorage {
 
   @override
   Future<Null> updateUser(int userIndex, Update<User> update) async {
-    _users = _users.mapAt(userIndex, update);
+    _users[userIndex] = update(_users[userIndex]);
+    return null;
   }
 }
