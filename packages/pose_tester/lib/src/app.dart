8c8694c6a6779145ef946be581c32f059c619ed5
Ramon Brullo <kingvanti650@gmail.com>
2024-12-04 14:09:35 +0100

    feat(pose_tester): display image list

--- packages/pose_tester/lib/src/app.dart
+++ packages/pose_tester/lib/src/app.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fpdart/fpdart.dart' hide State;
 
 class PoseTesterApp extends StatefulWidget {
   const PoseTesterApp({super.key});
@@ -8,11 +13,71 @@ class PoseTesterApp extends StatefulWidget {
 }
 
 class _PoseTesterAppState extends State<PoseTesterApp> {
+  IList<String> imageNames = const IList.empty();
+  Option<String> selectedImageName = none();
+
+  void loadImages() async {
+    final Map<String, dynamic> manifestContent =
+        jsonDecode(await rootBundle.loadString('AssetManifest.json'));
+
+    setState(() {
+      imageNames = manifestContent.keys
+          .where((it) => it.contains("test_image"))
+          .map((it) => it.split("/").last)
+          .toIList();
+      selectedImageName = Some(imageNames.first);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadImages();
+  }
+
+  void selectImage(String name) {
+    setState(() {
+      selectedImageName = Some(name);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final highlightedButtonStyle = ElevatedButton.styleFrom(
+        backgroundColor: Color.fromRGBO(200, 200, 240, 1));
+
     return MaterialApp(
       title: "Pose tester",
-      home: Placeholder(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Pose tester"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: Placeholder()),
+            SizedBox(
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: imageNames
+                    .map((name) => ElevatedButton(
+                          onPressed: () {
+                            selectImage(name);
+                          },
+                          style: Some(name) == selectedImageName
+                              ? highlightedButtonStyle
+                              : null,
+                          child: Text(name),
+                        ))
+                    .toList(),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
