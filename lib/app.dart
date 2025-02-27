fhstp/ergo4all

commit 7642e5cb6ad5b7f57fc787b1ebeefed043706ac7

misc: use flutter monorepo

pubspec.yaml
@@ -18,8 +18,17 @@ publish_to: "none" # Remove this line if you wish to publish to pub.dev
 # of the product and file versions while build-number is used as the build suffix.
 version: 1.30.1
 
+workspace:
+  - ./packages/common
+  - ./packages/custom_locale
+  - ./packages/pose
+  - ./packages/pose_analysis
+  - ./packages/rula
+  - ./packages/user_management
+  - ./packages/pose_tester
+
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
 
 # Dependencies specify other packages that your package needs in order to work.
 # To automatically upgrade your package dependencies to the latest versions

packages/common/pubspec.yaml
@@ -2,7 +2,9 @@ name: common
 description: Common logic which can be used in all project packages.
 
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
+
+resolution: workspace
 
 dependencies:
   flutter:

packages/custom_locale/pubspec.yaml
@@ -2,7 +2,9 @@ name: custom_locale
 description: Storage logic for local custom locale.
 
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
+
+resolution: workspace
 
 dependencies:
   flutter:

packages/pose/pubspec.yaml
@@ -2,7 +2,9 @@ name: pose
 description: Pose detection files.
 
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
+
+resolution: workspace
 
 dependencies:
   flutter:

packages/pose_analysis/pubspec.yaml
@@ -2,7 +2,9 @@ name: pose_analysis
 description: Pose analysis
 
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
+
+resolution: workspace
 
 dependencies:
   flutter:

packages/pose_tester/pubspec.yaml
@@ -4,7 +4,10 @@ publish_to: "none"
 version: 1.0.0
 
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
+
+resolution: workspace
+
 dependencies:
   flutter:
     sdk: flutter

packages/rula/pubspec.yaml
@@ -2,7 +2,9 @@ name: rula
 description: Calculation logic for rula scoring
 
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
+
+resolution: workspace
 
 dependencies:
   flutter:

packages/user_management/pubspec.yaml
@@ -2,7 +2,9 @@ name: user_management
 description: User management logic.
 
 environment:
-  sdk: ">=3.4.3 <4.0.0"
+  sdk: ^3.6.0
+
+resolution: workspace
 
 dependencies:
   flutter:

commit af8f49cca8f748eb5d936bf7df67559e4a162de9

fix: broken localization
In 7642e5cb6ad5b7f57fc787b1ebeefed043706ac7 we updated to flutter workspaces. But this breaks flutter_gen and thus localization (See https://github.com/flutter/flutter/issues/160151). So we migrated to use the new generation workflow.

https://docs.flutter.dev/release/breaking-changes/flutter-generate-i10n-source

.gitignore
@@ -49,3 +49,6 @@ app.*.map.json
 linux
 windows
 macos
+
+# Generated files
+**/lib/**/gen

l10n.yaml
@@ -1,3 +1,4 @@
 arb-dir: l10n
 template-arb-file: app_en.arb
-output-localization-file: app_localizations.dart
+output-dir: lib/gen/i18n
+synthetic-package: false

lib/app.dart
@@ -15,7 +15,7 @@ import 'package:ergo4all/theme.dart';
 import 'package:ergo4all/welcome/screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 
 class Ergo4AllApp extends StatefulWidget {
   const Ergo4AllApp({super.key});

lib/common/intro.dart
@@ -1,8 +1,8 @@
 import 'package:ergo4all/common/app_bar.dart';
 import 'package:ergo4all/common/routes.dart';
 import 'package:ergo4all/common/screen_content.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class IntroPage {

lib/home/screen.dart
@@ -3,6 +3,7 @@ import 'package:ergo4all/common/routes.dart';
 import 'package:ergo4all/common/screen_content.dart';
 import 'package:ergo4all/common/shimmer_box.dart';
 import 'package:ergo4all/common/snack.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:ergo4all/home/pick_video_dialog.dart';
 import 'package:ergo4all/home/session_start_dialog.dart';
 import 'package:ergo4all/home/show_tutorial_dialog.dart';
@@ -10,7 +11,6 @@ import 'package:ergo4all/home/types.dart';
 import 'package:ergo4all/home/user_welcome_header.dart';
 import 'package:ergo4all/home/viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:user_management/user_management.dart';
 

lib/home/user_welcome_header.dart
@@ -1,6 +1,6 @@
 import 'package:ergo4all/common/header.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:user_management/user_management.dart';
 
 /// Displays a welcome header for a given [User].

lib/onboarding/expert_intro_screen.dart
@@ -1,7 +1,7 @@
 import 'package:ergo4all/common/header.dart';
 import 'package:ergo4all/common/intro.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ExpertIntroScreen extends StatelessWidget {
   const ExpertIntroScreen({super.key});

lib/onboarding/non_expert_intro_screen.dart
@@ -1,7 +1,7 @@
 import 'package:ergo4all/common/header.dart';
 import 'package:ergo4all/common/intro.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 
 class NonExpertIntroScreen extends StatelessWidget {
   const NonExpertIntroScreen({super.key});

lib/onboarding/pick_language_screen.dart
@@ -5,7 +5,7 @@ import 'package:ergo4all/common/routes.dart';
 import 'package:ergo4all/common/screen_content.dart';
 import 'package:ergo4all/common/spacing.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 
 class PickLanguageScreen extends StatelessWidget {
   const PickLanguageScreen({super.key});

lib/onboarding/pre_intro_screen.dart
@@ -4,7 +4,7 @@ import 'package:ergo4all/common/header.dart';
 import 'package:ergo4all/common/screen_content.dart';
 import 'package:ergo4all/common/spacing.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 
 class PreIntroScreen extends StatelessWidget {
   const PreIntroScreen({super.key});

lib/onboarding/pre_user_creator_screen.dart
@@ -4,7 +4,7 @@ import 'package:ergo4all/common/routes.dart';
 import 'package:ergo4all/common/screen_content.dart';
 import 'package:ergo4all/common/spacing.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:user_management/user_management.dart';
 
 final _defaultUser = makeUserFromName("Ergo-fan");

lib/onboarding/terms_of_use_screen.dart
@@ -4,7 +4,7 @@ import 'package:ergo4all/common/routes.dart';
 import 'package:ergo4all/common/screen_content.dart';
 import 'package:ergo4all/common/spacing.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class TermsOfUseScreen extends HookWidget {

lib/onboarding/user_creation_form.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:user_management/user_management.dart';
 

lib/onboarding/user_creator_screen.dart
@@ -4,7 +4,7 @@ import 'package:ergo4all/common/routes.dart';
 import 'package:ergo4all/common/screen_content.dart';
 import 'package:ergo4all/onboarding/user_creation_form.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:user_management/user_management.dart';
 

pubspec.yaml
@@ -105,8 +105,6 @@ flutter:
   assets:
     - assets/images/logos/
 
-  generate: true
-
   # An image asset can refer to one or more resolution-specific "variants", see
   # https://flutter.dev/assets-and-images/#resolution-aware
 

test/widget/terms_of_use_screen_test.dart
@@ -1,6 +1,6 @@
 import 'package:ergo4all/onboarding/terms_of_use_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {

test/widget/user_creation_form_test.dart
@@ -1,6 +1,6 @@
 import 'package:ergo4all/onboarding/user_creation_form.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:user_management/user_management.dart';
 

test/widget/user_welcome_header_test.dart
@@ -2,7 +2,7 @@ import 'package:ergo4all/home/user_welcome_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:user_management/user_management.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 
 void main() {
   Widget buildTestWidget(User user) {