0b117b38bb5908fe37edd10cea4493f7ea936868
Ramon Brullo <kingvanti650@gmail.com>
2025-02-28 10:59:23 +0100

    feat(pose_vis): use 2d vis style for 3d
    We now use the same vis logic as for 2d for 3d

--- packages/pose_vis/lib/src/pose_3d_painter.dart
+++ packages/pose_vis/lib/src/pose_3d_painter.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pose/pose.dart';
+import 'package:pose_vis/src/painting.dart';
 
 /// [CustomPainter] for [Pose] objects. Intended to be overlayed over the image
 /// which produced the pose.
@@ -15,15 +16,8 @@ class Pose3dPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.clipRect(Rect.fromLTRB(0, 0, size.width, size.height));
-    final jointPaint = Paint()..color = Colors.red;
-    final bonePaint = Paint()
-      ..color = Colors.green
-      ..strokeWidth = 5;
-
-    Offset? tryGetPosOf(KeyPoints keyPoint) {
-      final landmark = pose[keyPoint];
-      if (visibilityOf(landmark!) < 0.9) return null;
+    Offset tryGetPosOf(KeyPoints keyPoint) {
+      final landmark = pose[keyPoint]!;
       final position = posOf(landmark).xy;
       return Offset(
         size.width * (position.x / imageSize.width),
@@ -31,50 +25,7 @@ class Pose3dPainter extends CustomPainter {
       );
     }
 
-    void drawJoint(KeyPoints keyPoint) {
-      final pos = tryGetPosOf(keyPoint);
-      if (pos != null) canvas.drawCircle(pos, 7, jointPaint);
-    }
-
-    void drawBone((KeyPoints, KeyPoints) bone) {
-      final (from, to) = bone;
-
-      final fromPos = tryGetPosOf(from);
-      if (fromPos == null) return;
-
-      final toPos = tryGetPosOf(to);
-      if (toPos == null) return;
-
-      canvas.drawLine(fromPos, toPos, bonePaint);
-    }
-
-    [
-      (KeyPoints.rightPalm, KeyPoints.rightElbow),
-      (KeyPoints.leftPalm, KeyPoints.leftElbow),
-      (KeyPoints.rightElbow, KeyPoints.rightShoulder),
-      (KeyPoints.leftElbow, KeyPoints.leftShoulder),
-      (KeyPoints.rightShoulder, KeyPoints.leftShoulder),
-      (KeyPoints.rightHip, KeyPoints.leftHip),
-      (KeyPoints.rightHip, KeyPoints.rightKnee),
-      (KeyPoints.leftHip, KeyPoints.leftKnee),
-      (KeyPoints.rightKnee, KeyPoints.rightAnkle),
-      (KeyPoints.leftKnee, KeyPoints.leftAnkle),
-    ].forEach(drawBone);
-
-    [
-      KeyPoints.leftShoulder,
-      KeyPoints.rightShoulder,
-      KeyPoints.leftElbow,
-      KeyPoints.rightElbow,
-      KeyPoints.leftPalm,
-      KeyPoints.rightPalm,
-      KeyPoints.leftHip,
-      KeyPoints.rightHip,
-      KeyPoints.leftKnee,
-      KeyPoints.rightKnee,
-      KeyPoints.leftAnkle,
-      KeyPoints.rightAnkle,
-    ].forEach(drawJoint);
+    paintPose(canvas, tryGetPosOf, getJointColorFromIndex);
   }
 
   @override
