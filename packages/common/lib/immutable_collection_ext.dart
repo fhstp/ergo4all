d820641754b8eb6ca7200211b41d8ad7a842e892
Ramon Brullo <kingvanti650@gmail.com>
2025-02-28 11:18:22 +0100

    style(common): fix analysis warnings

--- packages/common/lib/immutable_collection_ext.dart
+++ packages/common/lib/immutable_collection_ext.dart
@@ -1,7 +1,9 @@
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
+/// Utilities for [IMap].
 extension IMapExt<Key, Value> on IMap<Key, Value> {
-  /// Maps the entries in this [IMap] by applying [f] to all values, but keeping the keys unchanged.
+  /// Maps the entries in this [IMap] by applying [f] to all values, but
+  /// keeping the keys unchanged.
   IMap<Key, Mapped> mapValues<Mapped>(Mapped Function(Value value) f) =>
       map((key, value) => MapEntry(key, f(value)));
 }
