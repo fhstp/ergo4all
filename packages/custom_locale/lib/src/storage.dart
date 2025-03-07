6e32f5fb7db768fe98e689f7609722a329eb388e
Ramon Brullo <kingvanti650@gmail.com>
2025-03-07 11:14:46 +0100

    docs(custom_locale): add function docs

--- packages/custom_locale/lib/src/storage.dart
+++ packages/custom_locale/lib/src/storage.dart
@@ -2,14 +2,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 final SharedPreferencesAsync _sharedPrefs = SharedPreferencesAsync();
 
+/// Puts a [value] into shared preferences using the given [key].
 Future<void> putStringIntoPrefs(String key, String value) async {
   await _sharedPrefs.setString(key, value);
 }
 
+/// Attempts to get a [String] value from shared preferences using the given
+/// [key]. If no value is found, returns [Null].
 Future<String?> tryGetString(String key) {
   return _sharedPrefs.getString(key);
 }
 
+/// Attempts to get a value from shared preferences using the given [key].
+/// The value will be parsed using the given [parse] function. If no value
+/// is found, returns [Null].
 Future<T?> tryGetItemFromPrefs<T>(
   String key,
   T Function(String pref) parse,
