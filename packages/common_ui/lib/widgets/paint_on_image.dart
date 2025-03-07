57eab2d4da7a2680398a2216ed53995e5ac27580
Ramon Brullo <kingvanti650@gmail.com>
2025-03-07 12:23:06 +0100

    docs(common_ui): add summaries

--- packages/common_ui/lib/widgets/paint_on_image.dart
+++ packages/common_ui/lib/widgets/paint_on_image.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+/// Widget for painting on top of another widget using a [CustomPainter].
+/// The painter will be scaled to fit the base widget.
 class PaintOnWidget extends StatelessWidget {
+  /// Constructs a [PaintOnWidget].
   const PaintOnWidget({
     required this.base,
     super.key,
     this.painter,
   });
 
+  /// The widget to paint onto.
   final Widget base;
+
+  /// The painter to use for painting. Will not do any painting if [Null].
   final CustomPainter? painter;
 
   @override
