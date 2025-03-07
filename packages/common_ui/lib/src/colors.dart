5b7b4297d429da4c92fc88856b64fbf824b44a31
Ramon Brullo <kingvanti650@gmail.com>
2025-03-07 10:02:49 +0100

    misc(common_ui): add colors

---
+++ packages/common_ui/lib/src/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Dark blue color.
+const darkBlue = Color(0xFF0B3954);
+
+/// Medium dark blue color.
+const blue = Color(0xFF087E8B);
+
+/// Pale light blue color.
+const lightBlue = Color(0xFFBFD7EA);
+
+/// Pale light red color.
+const lightRed = Color(0xFFFF5A5F);
+
+/// Strong red color.
+const red = Color(0xFFC81D25);
