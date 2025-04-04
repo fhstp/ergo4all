13137f92f4db4d0141563c60668e610396c32871
Ramon Brullo <kingvanti650@gmail.com>
2025-04-04 08:50:28 +0200

    feat(app): add circle graphic to welcome screen

--- apps/ergo4all/lib/welcome/screen.dart
+++ apps/ergo4all/lib/welcome/screen.dart
@@ -7,6 +7,7 @@ import 'package:ergo4all/welcome/version_display.dart';
 import 'package:ergo4all/welcome/viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_svg/svg.dart';
 
 /// Top-level widget for the welcome screen.
 class WelcomeScreen extends HookWidget {
@@ -30,62 +31,76 @@ class WelcomeScreen extends HookWidget {
     }, [null]);
 
     return Scaffold(
-      body: ScreenContent(
-        child: Column(
-          children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Image(image: CustomImages.logoRed),
-                  const SizedBox(
-                    height: largeSpace,
-                  ),
-                  Text("Powered by"),
-                  const SizedBox(
-                    height: smallSpace,
-                  ),
-                  const Image(
-                    image: CustomImages.logoAk,
-                    height: 200,
-                  ),
-                  const SizedBox(
-                    height: largeSpace,
-                  ),
-                  Text("Project partners"),
-                  const SizedBox(
-                    height: smallSpace,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Image(
-                        image: CustomImages.logoTUWien,
-                        height: 100,
-                      ),
-                      SizedBox(
-                        width: largeSpace,
-                      ),
-                      const Image(
-                        image: CustomImages.logoFhStp,
-                        height: 100,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: largeSpace,
-                  ),
-                  TimedLoadingBar(
-                    duration: const Duration(seconds: 3),
-                    completed: navigateToNextScreen,
-                  )
-                ],
-              ),
+      body: Stack(children: [
+        Positioned(
+          top: 0,
+          left: 0,
+          right: 0,
+          child: FittedBox(
+            fit: BoxFit.cover,
+            child: SvgPicture.asset(
+              'assets/images/top_circle_large.svg',
+              package: "common_ui",
             ),
-            VersionDisplay(version: uiState.projectVersion)
-          ],
+          ),
         ),
-      ),
+        ScreenContent(
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Image(image: CustomImages.logoRed),
+                    const SizedBox(
+                      height: largeSpace,
+                    ),
+                    Text("Powered by"),
+                    const SizedBox(
+                      height: smallSpace,
+                    ),
+                    const Image(
+                      image: CustomImages.logoAk,
+                      height: 200,
+                    ),
+                    const SizedBox(
+                      height: largeSpace,
+                    ),
+                    Text("Project partners"),
+                    const SizedBox(
+                      height: smallSpace,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Image(
+                          image: CustomImages.logoTUWien,
+                          height: 100,
+                        ),
+                        SizedBox(
+                          width: largeSpace,
+                        ),
+                        const Image(
+                          image: CustomImages.logoFhStp,
+                          height: 100,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: largeSpace,
+                    ),
+                    TimedLoadingBar(
+                      duration: const Duration(seconds: 3),
+                      completed: navigateToNextScreen,
+                    )
+                  ],
+                ),
+              ),
+              VersionDisplay(version: uiState.projectVersion)
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
