4dd95db63342ddd4166839946dad7f33f90e782d
Ramon Brullo <kingvanti650@gmail.com>
2025-04-10 12:43:58 +0200

    feat(app): display scenario description

--- apps/ergo4all/lib/scenario/scenario_detail_screen.dart
+++ apps/ergo4all/lib/scenario/scenario_detail_screen.dart
@@ -25,14 +25,36 @@ class ScenarioDetailScreen extends StatelessWidget {
       Scenario.conveyorBelt => localizations.scenario_conveyor_summary,
     };
 
+    final description = switch (scenario) {
+      Scenario.liftAndCarry =>
+        localizations.scenario_lift_and_carry_description,
+      Scenario.pull => localizations.scenario_pull_description,
+      Scenario.seated => localizations.scenario_seated_description,
+      Scenario.packaging => localizations.scenario_packaging_description,
+      Scenario.standingCNC => localizations.scenario_CNC_description,
+      Scenario.standingAssembly => localizations.scenario_assembly_description,
+      Scenario.ceiling => localizations.scenario_ceiling_description,
+      Scenario.lift25 => localizations.scenario_lift_and_carry_description,
+      Scenario.conveyorBelt => localizations.scenario_conveyor_description,
+    };
+
     return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Text(
             summary,
             style: h4Style.copyWith(color: cardinal),
             textAlign: TextAlign.center,
+          ),
+          Text(
+            localizations.common_description,
+            style: paragraphHeader,
+            textAlign: TextAlign.start,
+          ),
+          Text(
+            description,
+            textAlign: TextAlign.start,
           )
         ],
       ),
