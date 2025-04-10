7094491ed9522a4a0a164f771ec11ab5da35216f
Ramon Brullo <kingvanti650@gmail.com>
2025-04-10 11:02:26 +0200

    feat: add scenario summaries
    Add scenario summaries from excel and use them for button labels on scenario choice screen. They are actually far too long so we need even shorter summaries. @ThiccWitch

--- apps/ergo4all/lib/scenario/scenario_choice_screen.dart
+++ apps/ergo4all/lib/scenario/scenario_choice_screen.dart
@@ -12,6 +12,19 @@ class ScenarioChoiceScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
 
+    String titleFor(Scenario scenario) => switch (scenario) {
+          Scenario.liftAndCarry25 =>
+            localizations.scenario_lift_and_carry_summary,
+          Scenario.pull150 => localizations.scenario_pull_summary,
+          Scenario.seated => localizations.scenario_seated_summary,
+          Scenario.packaging => localizations.scenario_packaging_summary,
+          Scenario.standingCNC => localizations.scenario_CNC_summary,
+          Scenario.standingAssembly => localizations.scenario_assembly_summary,
+          Scenario.ceiling => localizations.scenario_ceiling_summary,
+          Scenario.lift25 => localizations.scenario_lift_and_carry_summary,
+          Scenario.conveyorBelt => localizations.scenario_conveyor_summary,
+        };
+
     return Scaffold(
       body: Column(
         children: [
@@ -28,7 +41,7 @@ class ScenarioChoiceScreen extends StatelessWidget {
                 final scenario = Scenario.values[i];
                 return ElevatedButton(
                   style: paleTextButtonStyle,
-                  child: Text(scenario.name),
+                  child: Text(titleFor(scenario)),
                   onPressed: () {},
                 );
               },
