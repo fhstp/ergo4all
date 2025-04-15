5938394c8b2d1524fbdd0767c99f7f62212c7ed1
Ramon Brullo <kingvanti650@gmail.com>
2025-04-15 09:48:58 +0200

    docs(app): add dart docs

--- apps/ergo4all/lib/home/user_welcome_header.dart
+++ apps/ergo4all/lib/home/user_welcome_header.dart
@@ -5,8 +5,10 @@ import 'package:user_management/user_management.dart';
 
 /// Displays a welcome header for a given [User].
 class UserWelcomeHeader extends StatelessWidget {
+  /// Creates a welcome header for the given [user].
   const UserWelcomeHeader(this.user, {super.key});
 
+  /// The user to display a message for.
   final User user;
 
   @override
