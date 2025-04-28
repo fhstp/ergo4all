c6eddae99827f54cfe82b1c1cef1e4df88c1841d
Andreas Kaiser <it241512@fhstp.ac.at>
2025-04-28 11:12:28 +0200

    feat(app): Zusätzliche Menüpunkte hinzugefügt
    Im Menü sieht man nun die Punkte:
    - Home
    - User ändern
    - Sprache ändern
    - Impressum
    - Datenschutz
    - Alle Daten löschen
    
    Resolves #76

--- apps/ergo4all/lib/home/menu_dialog.dart
+++ apps/ergo4all/lib/home/menu_dialog.dart
@@ -1,6 +1,8 @@
 import 'package:common_ui/theme/colors.dart';
 import 'package:common_ui/theme/styles.dart';
 import 'package:ergo4all/common/routes.dart';
+import 'package:ergo4all/common/snack.dart';
+import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
 Widget _makeOptionButton(String key, String text, void Function() onPressed) {
@@ -18,8 +20,14 @@ Widget _makeOptionButton(String key, String text, void Function() onPressed) {
 /// Shows a dialog with common operations that users might want to do from
 /// the home screen.
 Future<void> showHomeMenuDialog(BuildContext context) {
+  final localizations = AppLocalizations.of(context)!;
+
   void goToLanguage() {
     Navigator.of(context).pushReplacementNamed(Routes.language.path);
+  }
+
+  void goToHome() {
+    Navigator.of(context).pushReplacementNamed(Routes.home.path);
   }
 
   return showDialog<void>(
@@ -30,7 +38,24 @@ Future<void> showHomeMenuDialog(BuildContext context) {
         horizontal: 20,
       ),
       children: [
-        _makeOptionButton('button-lang', 'Change language', goToLanguage),
+        _makeOptionButton('button-home',localizations.menu_home_label, goToHome),
+        _makeOptionButton('button-user',localizations.menu_user_label, () {
+          Navigator.of(context).pop();
+          showNotImplementedSnackbar(context);
+        }),
+        _makeOptionButton('button-lang',localizations.menu_language_label, goToLanguage),
+        _makeOptionButton('button-imprint',localizations.menu_imprint_label, () {
+          Navigator.of(context).pop();
+          showNotImplementedSnackbar(context);
+        }),
+        _makeOptionButton('button-privacy',localizations.menu_privacy_label, () {
+          Navigator.of(context).pop();
+          showNotImplementedSnackbar(context);
+        }),
+        _makeOptionButton('button-delete',localizations.menu_delete_data_label, () {
+          Navigator.of(context).pop();
+          showNotImplementedSnackbar(context);
+        }),
       ],
     ),
   );
