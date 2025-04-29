eda78de190b0d7530630c1131c61993c151ddb47
Andreas Kaiser <it241512@fhstp.ac.at>
2025-04-30 01:36:58 +0200

    feat(app): Lokalisierung der Menüpunkte
    Die Betitelung der einzelnen Menüpunkte lokalisiert.
    
    Resolves #75

--- apps/ergo4all/lib/home/menu_dialog.dart
+++ apps/ergo4all/lib/home/menu_dialog.dart
@@ -5,9 +5,8 @@ import 'package:ergo4all/common/snack.dart';
 import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
-Widget _makeOptionButton(String key, String text, void Function() onPressed) {
+Widget _makeOptionButton(String text, void Function() onPressed) {
   return SimpleDialogOption(
-    key: Key(key),
     onPressed: onPressed,
     child: Text(
       text,
@@ -38,21 +37,21 @@ Future<void> showHomeMenuDialog(BuildContext context) {
         horizontal: 20,
       ),
       children: [
-        _makeOptionButton('button-home',localizations.menu_home_label, goToHome),
-        _makeOptionButton('button-user',localizations.menu_user_label, () {
+        _makeOptionButton(localizations.menu_home_label, goToHome),
+        _makeOptionButton(localizations.menu_user_label, () {
           Navigator.of(context).pop();
           showNotImplementedSnackbar(context);
         }),
-        _makeOptionButton('button-lang',localizations.menu_language_label, goToLanguage),
-        _makeOptionButton('button-imprint',localizations.menu_imprint_label, () {
+        _makeOptionButton(localizations.menu_language_label, goToLanguage),
+        _makeOptionButton(localizations.menu_imprint_label, () {
           Navigator.of(context).pop();
           showNotImplementedSnackbar(context);
         }),
-        _makeOptionButton('button-privacy',localizations.menu_privacy_label, () {
+        _makeOptionButton(localizations.menu_privacy_label, () {
           Navigator.of(context).pop();
           showNotImplementedSnackbar(context);
         }),
-        _makeOptionButton('button-delete',localizations.menu_delete_data_label, () {
+        _makeOptionButton(localizations.menu_delete_data_label, () {
           Navigator.of(context).pop();
           showNotImplementedSnackbar(context);
         }),
