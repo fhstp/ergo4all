97925a45debb2cfcae2e29d870e67516066377d8
Ramon Brullo <kingvanti650@gmail.com>
2025-05-08 10:44:06 +0200

    refactor(app): body part display
    Extract parameterized function. This will make it easier to display colored body parts

--- apps/ergo4all/lib/results/results_overview/body_score_display.dart
+++ apps/ergo4all/lib/results/results_overview/body_score_display.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
+enum _BodyPart {
+  head('head'),
+  leftHand('left_hand'),
+  leftLeg('left_leg'),
+  leftLowerArm('left_lower_arm'),
+  leftUpperArm('left_upper_arm'),
+  rightHand('right_hand'),
+  rightLeg('right_leg'),
+  rightLowerArm('right_lower_arm'),
+  rightUpperArm('right_upper_arm'),
+  upperBody('upper_body'),
+  ;
+
+  const _BodyPart(this.fileName);
+
+  final String fileName;
+}
+
+Widget _getBodyPartImage(_BodyPart bodyPart) {
+  return Image.asset('assets/images/puppet/${bodyPart.fileName}_blue.png');
+}
+
 /// Displays the aggregate score of a user using a puppet.
 class BodyScoreDisplay extends StatelessWidget {
   ///
@@ -9,16 +31,16 @@ class BodyScoreDisplay extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image.asset('assets/images/puppet/head_blue.png'),
-        Image.asset('assets/images/puppet/left_leg_blue.png'),
-        Image.asset('assets/images/puppet/left_upper_arm_blue.png'),
-        Image.asset('assets/images/puppet/left_lower_arm_blue.png'),
-        Image.asset('assets/images/puppet/left_hand_blue.png'),
-        Image.asset('assets/images/puppet/upper_body_blue.png'),
-        Image.asset('assets/images/puppet/right_leg_blue.png'),
-        Image.asset('assets/images/puppet/right_upper_arm_blue.png'),
-        Image.asset('assets/images/puppet/right_lower_arm_blue.png'),
-        Image.asset('assets/images/puppet/right_hand_blue.png'),
+        _getBodyPartImage(_BodyPart.head),
+        _getBodyPartImage(_BodyPart.leftLeg),
+        _getBodyPartImage(_BodyPart.leftUpperArm),
+        _getBodyPartImage(_BodyPart.leftLowerArm),
+        _getBodyPartImage(_BodyPart.leftHand),
+        _getBodyPartImage(_BodyPart.upperBody),
+        _getBodyPartImage(_BodyPart.rightLeg),
+        _getBodyPartImage(_BodyPart.rightUpperArm),
+        _getBodyPartImage(_BodyPart.rightLowerArm),
+        _getBodyPartImage(_BodyPart.rightHand),
       ],
     );
   }
