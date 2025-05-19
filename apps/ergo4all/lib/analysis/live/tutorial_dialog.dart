ff59e3733b1ced7bf9dbbad9efeb7dbdcd91cb6e
Ramon Brullo <kingvanti650@gmail.com>
2025-05-19 15:58:30 +0200

    feat(app): add second page of recording tutorial
    Replaces the tutorial dialog with a stateful widget to track on what page we are and add logic to display the second on when we press 'next'

--- apps/ergo4all/lib/analysis/live/tutorial_dialog.dart
+++ apps/ergo4all/lib/analysis/live/tutorial_dialog.dart
@@ -4,41 +4,66 @@ import 'package:common_ui/theme/styles.dart';
 import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
+class _TutorialDialog extends StatefulWidget {
+  const _TutorialDialog();
+
+  @override
+  State<_TutorialDialog> createState() => _TutorialDialogState();
+}
+
+class _TutorialDialogState extends State<_TutorialDialog> {
+  int pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final localizations = AppLocalizations.of(context)!;
+
+    void onNextPressed() {
+      if (pageIndex == 0) {
+        setState(() {
+          pageIndex = 1;
+        });
+      } else {
+        Navigator.of(context).pop();
+      }
+    }
+
+    final text = switch (pageIndex) {
+      0 => localizations.tutorial_dialog_page_1,
+      _ => localizations.tutorial_dialog_page_2
+    };
+
+    return Dialog(
+      backgroundColor: tarawera.withAlpha(230),
+      child: IntrinsicHeight(
+        child: Padding(
+          padding: const EdgeInsets.all(mediumSpace),
+          child: Column(
+            children: [
+              Text(
+                text,
+                textAlign: TextAlign.center,
+                style: staticBodyStyle.copyWith(color: white),
+              ),
+              const SizedBox(height: largeSpace),
+              ElevatedButton(
+                onPressed: onNextPressed,
+                style: primaryTextButtonStyle,
+                child: Text(localizations.common_next),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 /// Shows a tutorial in a [Dialog] which explains to the user how to
 /// record videos. The returned [Future] completes when the dialog is closed.
 Future<void> showTutorialDialog(BuildContext context) {
   return showDialog(
     context: context,
-    builder: (context) {
-      final localizations = AppLocalizations.of(context)!;
-
-      void closeDialog() {
-        Navigator.of(context).pop();
-      }
-
-      return Dialog(
-        backgroundColor: tarawera.withAlpha(230),
-        child: IntrinsicHeight(
-          child: Padding(
-            padding: const EdgeInsets.all(mediumSpace),
-            child: Column(
-              children: [
-                Text(
-                  localizations.tutorial_dialog_page_1,
-                  textAlign: TextAlign.center,
-                  style: staticBodyStyle.copyWith(color: white),
-                ),
-                const SizedBox(height: largeSpace),
-                ElevatedButton(
-                  onPressed: closeDialog,
-                  style: primaryTextButtonStyle,
-                  child: Text(localizations.common_next),
-                ),
-              ],
-            ),
-          ),
-        ),
-      );
-    },
+    builder: (context) => const _TutorialDialog(),
   );
 }
