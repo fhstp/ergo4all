c25d83d97408dac493637303ba2690598b7c6285
Andreas Kaiser <it241512@fhstp.ac.at>
2025-05-19 11:56:31 +0200

    feat(app): Erstversion Tip Detail Screen
    Der Tip Detail Screen enthält vorerst einen Titel und eine genaue Beschreibung. Alle Texte wurden lokalisiert.
    Der untere Part des Screen ist noch offen.

--- apps/ergo4all/lib/tips/tip_detail_screen.dart
+++ apps/ergo4all/lib/tips/tip_detail_screen.dart
@@ -3,7 +3,6 @@ import 'package:common_ui/theme/styles.dart';
 import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:ergo4all/tips/domain.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class TipDetailScreen extends StatelessWidget {
   const TipDetailScreen({super.key});
