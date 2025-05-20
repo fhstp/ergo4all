96378dece434ed33d7e0f650aed52eaeb3d9759d
Ramon Brullo <kingvanti650@gmail.com>
2025-05-20 09:38:18 +0200

    feat(pose_vis): configure pose paint color
    It is now possible to configure the color of the 3d pose painter

--- packages/pose_vis/lib/src/pose_3d_painter.dart
+++ packages/pose_vis/lib/src/pose_3d_painter.dart
@@ -6,13 +6,21 @@ import 'package:pose_vis/src/painting.dart';
 /// which produced the pose.
 class Pose3dPainter extends CustomPainter {
   /// Creates a pose painter.
-  Pose3dPainter({required this.pose, required this.imageSize, super.repaint});
+  Pose3dPainter({
+    required this.pose,
+    required this.imageSize,
+    this.color,
+    super.repaint,
+  });
 
   /// The pose to visualize.
   final Pose pose;
 
   /// The size of the original image from which the pose was estimated,
   final Size imageSize;
+
+  /// Color to use for joints. If omitted uses the "rainbow" color scheme.
+  final Color? color;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -25,7 +33,11 @@ class Pose3dPainter extends CustomPainter {
       );
     }
 
-    paintPose(canvas, tryGetPosOf, getJointColorFromIndex);
+    paintPose(
+      canvas,
+      tryGetPosOf,
+      color != null ? (_) => color! : getJointColorFromIndex,
+    );
   }
 
   @override
