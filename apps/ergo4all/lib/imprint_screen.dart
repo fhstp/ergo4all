6b7475e80ec8175b4ae07bb07f8fc876543eeeb8
Ramon Brullo <kingvanti650@gmail.com>
2025-05-20 13:39:21 +0200

    feat(app): contact placeholders on imprint screen
    The imprint screen now has contact blocks for ak, fh and tu. A lot of the contact info is not filled out yet.

--- apps/ergo4all/lib/imprint_screen.dart
+++ apps/ergo4all/lib/imprint_screen.dart
@@ -1,6 +1,43 @@
+import 'package:common_ui/theme/spacing.dart';
 import 'package:common_ui/widgets/red_circle_top_bar.dart';
+import 'package:ergo4all/common/custom_images.dart';
 import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
+
+class _ContactBlock extends StatelessWidget {
+  const _ContactBlock({
+    required this.logo,
+    required this.companyName,
+    required this.companyAddress,
+    required this.contactPersonName,
+    required this.contactEmail,
+    required this.contactTelephone,
+    super.key,
+  });
+
+  final AssetImage logo;
+  final String companyName;
+  final String companyAddress;
+  final String contactPersonName;
+  final String contactEmail;
+  final String contactTelephone;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(image: logo, height: 35),
+        const SizedBox(height: mediumSpace),
+        Text(companyName),
+        Text(companyAddress),
+        Text(contactPersonName),
+        Text(contactEmail),
+        Text(contactTelephone),
+      ],
+    );
+  }
+}
 
 /// Screen for displaying information about project partners and contributors.
 class ImprintScreen extends StatelessWidget {
@@ -15,6 +52,42 @@ class ImprintScreen extends StatelessWidget {
       appBar: RedCircleAppBar(
         titleText: localizations.imprint,
         withBackButton: true,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: mediumSpace,
+          vertical: largeSpace,
+        ),
+        child: Column(
+          children: [
+            _ContactBlock(
+              logo: CustomImages.logoAk,
+              companyName: 'AK Niederösterreich',
+              companyAddress: '[Address]',
+              contactPersonName: '[Contact Name]',
+              contactEmail: '[Contact Email]',
+              contactTelephone: '[Contact Telephone]',
+            ),
+            SizedBox(height: largeSpace),
+            _ContactBlock(
+              logo: CustomImages.logoFhStp,
+              companyName: 'FH St. Pölten',
+              companyAddress: '[Address]',
+              contactPersonName: '[Contact Name]',
+              contactEmail: '[Contact Email]',
+              contactTelephone: '[Contact Telephone]',
+            ),
+            SizedBox(height: largeSpace),
+            _ContactBlock(
+              logo: CustomImages.logoTUWien,
+              companyName: 'TU Wien',
+              companyAddress: '[Address]',
+              contactPersonName: '[Contact Name]',
+              contactEmail: '[Contact Email]',
+              contactTelephone: '[Contact Telephone]',
+            ),
+          ],
+        ),
       ),
     );
   }
