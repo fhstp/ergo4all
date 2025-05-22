312c5031c1495ca3bc2feb0b1206c4e424f74820
Ramon Brullo <kingvanti650@gmail.com>
2025-05-22 11:24:41 +0200

    feat: overhaul language screen
    Adjust to better fit figma design. Removes the red title and centers items on the screen

--- apps/ergo4all/lib/language/screen.dart
+++ apps/ergo4all/lib/language/screen.dart
@@ -1,9 +1,7 @@
 import 'package:common_ui/theme/spacing.dart';
 import 'package:common_ui/theme/styles.dart';
 import 'package:custom_locale/custom_locale.dart';
-import 'package:ergo4all/common/app_bar.dart';
 import 'package:ergo4all/common/routes.dart';
-import 'package:ergo4all/common/screen_content.dart';
 import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:flutter/material.dart';
 
@@ -37,24 +35,22 @@ class PickLanguageScreen extends StatelessWidget {
     }
 
     return Scaffold(
-      appBar: makeCustomAppBar(
-        title: localizations.language_title,
-      ),
-      body: ScreenContent(
-        child: SafeArea(
+      body: SafeArea(
+        child: Align(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
                 localizations.language_header,
                 style: h3Style,
+                textAlign: TextAlign.center,
               ),
               const SizedBox(
                 height: largeSpace,
               ),
               languageButtonFor('Deutsch', const Locale('de')),
               const SizedBox(
-                height: mediumSpace,
+                height: largeSpace,
               ),
               languageButtonFor('English', const Locale('en')),
             ],
