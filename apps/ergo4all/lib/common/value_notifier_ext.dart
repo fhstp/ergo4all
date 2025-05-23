aaab38801699f83578eec81dae7239bc4b865a5e
Ramon Brullo <kingvanti650@gmail.com>
2025-05-23 09:16:14 +0200

    refactor: drop unused code

--- apps/ergo4all/lib/common/value_notifier_ext.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-extension UtilExt<T> on ValueNotifier<T> {
-  /// Updates this notifiers value by applying [f] to it.
-  void update(T Function(T) f) {
-    value = f(value);
-  }
-}
