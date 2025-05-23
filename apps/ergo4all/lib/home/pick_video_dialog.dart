6e48d0a531f7b77a671382a83ca514bd299f87e7
Ramon Brullo <kingvanti650@gmail.com>
2025-05-23 09:00:46 +0200

    refactor: drop unused code

--- apps/ergo4all/lib/home/pick_video_dialog.dart
+++
@@ -1,7 +0,0 @@
-import 'package:image_picker/image_picker.dart';
-
-/// Shows UI for a user to select a local video file from their gallery.
-/// Returns file videos [XFile] or `null` if none was selected.
-Future<XFile?> showVideoPickDialog() {
-  return ImagePicker().pickVideo(source: ImageSource.gallery);
-}
