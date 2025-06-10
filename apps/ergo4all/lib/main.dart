aa6139143246f62585bf205d780cfda2a45e8535
SantiagoCalvo <santiago.calvo4942@gmail.com>
2025-06-10 09:32:14 +0200

    feat: apply change request changes; inline object store class, improve docs, move UI logic out of domain objects

--- apps/ergo4all/lib/main.dart
+++ apps/ergo4all/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:ergo4all/app.dart';
-import 'package:ergo4all/storage/object_box.dart';
+import 'package:ergo4all/objectbox.g.dart';
 import 'package:ergo4all/storage/object_box_rula_session_repository.dart';
 import 'package:ergo4all/storage/rula_session_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final store = await ObjectBox.create();
-  final rulaSessionRepository = ObjectBoxRulaSessionRepository(store.store);
+  final docsDir = await getApplicationDocumentsDirectory();
+  final store = await openStore(directory: p.join(docsDir.path, 'rulaSessions-db'));
+  final rulaSessionRepository = ObjectBoxRulaSessionRepository(store);
 
   runApp(
     MultiProvider(
--- apps/ergo4all/lib/storage/rula_session.dart
+++ apps/ergo4all/lib/storage/rula_session.dart
@@ -4,9 +4,9 @@ import 'package:ergo4all/scenario/common.dart';
 /// data class to use in order to store rula sessions on device database
 class RulaSession {
   const RulaSession({required this.timestamp, required this.scenario, required this.timeline});
-  /// session timestamp
+  /// session timestamp: corresponds to the date and time at which the session was recorded.
   final int timestamp;
-  /// session timestamp
+  /// session scenario
   final Scenario scenario;
   /// session RulaTimeline
   final RulaTimeline timeline;
