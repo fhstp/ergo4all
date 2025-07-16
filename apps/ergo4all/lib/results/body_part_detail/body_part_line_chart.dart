be7014664a4dcbe574868df8044ea29b6dbe5431
Ramon Brullo <kingvanti650@gmail.com>
2025-07-16 12:47:50 +0200

    refactor: body part line graph
    Just clean up various small things. Extract variables, use utility functions, etc

--- apps/ergo4all/lib/results/body_part_detail/body_part_line_chart.dart
+++ apps/ergo4all/lib/results/body_part_detail/body_part_line_chart.dart
@@ -4,6 +4,16 @@ import 'package:ergo4all/results/rula_colors.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
+
+final _lineGray = Colors.grey.withValues(alpha: 0.2);
+
+final _hLine = FlLine(
+  color: _lineGray,
+  strokeWidth: 3,
+);
+
+const _noTitles = AxisTitles();
 
 /// Displays the normalized scores of a body-part over time using a
 /// 2D line-chart.
@@ -20,8 +30,13 @@ class BodyPartLineChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
+
     final colors = normalizedScores
         .map((score) => rulaColorFor(score, dark: true))
+        .toList();
+
+    final spots = normalizedScores
+        .mapWithIndex((score, i) => FlSpot(i.toDouble(), score))
         .toList();
 
     return LineChart(
@@ -29,53 +44,37 @@ class BodyPartLineChart extends StatelessWidget {
         gridData: FlGridData(
           drawVerticalLine: false,
           horizontalInterval: 0.5,
-          getDrawingHorizontalLine: (value) {
-            return FlLine(
-              color: Colors.grey.withValues(alpha: 0.2),
-              strokeWidth: 3,
-            );
-          },
+          getDrawingHorizontalLine: (value) => _hLine,
         ),
         titlesData: FlTitlesData(
-          rightTitles: const AxisTitles(),
-          topTitles: const AxisTitles(),
-          bottomTitles: const AxisTitles(),
+          rightTitles: _noTitles,
+          topTitles: _noTitles,
+          bottomTitles: _noTitles,
           leftTitles: AxisTitles(
             sideTitles: SideTitles(
               showTitles: true,
               interval: 0.5,
-              reservedSize: 60,
-              getTitlesWidget: (value, meta) {
-                return Text(
-                  value == 0.0
-                      ? localizations.results_score_low_short
-                      : value == 1.0
-                          ? localizations.results_score_high_short
-                          : '',
-                  style: infoText,
-                );
-              },
+              reservedSize: 80,
+              getTitlesWidget: (value, meta) => Text(
+                switch (value) {
+                  0.0 => localizations.results_score_low_short,
+                  1.0 => localizations.results_score_high_short,
+                  _ => ''
+                },
+                style: infoText,
+              ),
             ),
           ),
         ),
         borderData: FlBorderData(show: false),
         minX: 0,
         maxX: normalizedScores.length.toDouble() - 1,
-        minY: 0 - 0.01, // Adjusted to fit the grid
-        maxY: 1 + 0.01, // Adjusted to fit the grid
+        minY: 0,
+        maxY: 1,
         lineBarsData: [
           LineChartBarData(
-            spots: List.generate(
-              normalizedScores.length,
-              (i) => FlSpot(i.toDouble(), normalizedScores[i]),
-            ),
-            gradient: LinearGradient(
-              colors: colors,
-              stops: List.generate(
-                colors.length,
-                (index) => index / (colors.length - 1),
-              ),
-            ),
+            spots: spots,
+            gradient: LinearGradient(colors: colors),
             barWidth: 3,
             isStrokeCapRound: true,
             dotData: const FlDotData(show: false),
