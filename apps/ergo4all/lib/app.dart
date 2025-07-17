361889fd9c14a9a0a3246bd79502813ce2359934
Ramon Brullo <kingvanti650@gmail.com>
2025-07-17 10:14:52 +0200

    refactor: simplify project structure
    Move files for analysis screen up one level. They were inside a 'live' directory, because we thought there will be a 'non-live' analysis screen. This seems unlikely at this point, so we can simplify the dir structure

--- apps/ergo4all/lib/app.dart
+++ apps/ergo4all/lib/app.dart
@@ -2,7 +2,7 @@ import 'package:common/casting.dart';
 import 'package:common_ui/theme/theme.dart';
 import 'package:custom_locale/custom_locale.dart';
 import 'package:ergo4all/analysis/common.dart';
-import 'package:ergo4all/analysis/live/screen.dart';
+import 'package:ergo4all/analysis/screen.dart';
 import 'package:ergo4all/common/routes.dart';
 import 'package:ergo4all/gen/i18n/app_localizations.dart';
 import 'package:ergo4all/home/screen.dart';
