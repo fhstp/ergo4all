29c4bf5ea25af008cb68f7c0b935e8741e52f0a9
Ramon Brullo <kingvanti650@gmail.com>
2025-08-27 11:47:07 +0200

    docs: add dart docs

--- apps/ergo4all/lib/welcome/version_display.dart
+++ apps/ergo4all/lib/welcome/version_display.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fpdart/fpdart.dart';
 
+/// Displays the apps version if it is known.
 class VersionDisplay extends StatelessWidget {
+  ///
   const VersionDisplay({required this.version, super.key});
+
+  /// The apps version if known. Should be a semantic version, such as 1.0.3.
   final Option<String> version;
 
   @override
