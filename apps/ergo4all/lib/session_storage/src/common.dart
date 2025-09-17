489ef64f9abd81fa269d864587a1931f247029ac
Ramon Brullo <ramon.brullo@proton.me>
2025-09-17 11:05:15 +0200

    feat: optimize session choice screen
    We now only load session meta-data instead of all the session data on the session choice screen. This is both a speed up and helps keep ram usage down. We now only load the full session data once a session is selected

--- apps/ergo4all/lib/session_storage/src/common.dart
+++ apps/ergo4all/lib/session_storage/src/common.dart
@@ -1,12 +1,19 @@
 import 'package:ergo4all/common/rula_session.dart';
+import 'package:ergo4all/session_storage/src/fs.dart';
 
 /// Store for [RulaSession] objects.
 abstract class RulaSessionRepository {
   /// Stores a [RulaSession] object.
   Future<void> put(RulaSession session);
 
-  /// Retrieves all stored [RulaSession]s
-  Future<List<RulaSession>> getAll();
+  /// Attempts to load the [RulaSession] associated with the given
+  /// timestamp. If no such session is found `null` is returned.
+  Future<RulaSession?> getByTimestamp(int timestamp);
+
+  /// Retrieves meta data for all stored [RulaSession]s. If you want to
+  /// actually load the full session data use [getByTimestamp] with
+  /// [RulaSessionMeta.timestamp].
+  Future<List<RulaSessionMeta>> getAll();
 
   /// Deletes a [RulaSession] based on it's [timestamp].
   Future<void> deleteByTimestamp(int timestamp);
