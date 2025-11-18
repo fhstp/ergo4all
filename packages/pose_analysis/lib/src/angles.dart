f80a15c0599bd1c7a8977ec87147f74e238ac278
Ramon Brullo <ramon.brullo@proton.me>
2025-11-18 11:34:40 +0100

    use auto_rula key angles

--- packages/pose_analysis/lib/src/angles.dart
+++ packages/pose_analysis/lib/src/angles.dart
@@ -4,63 +4,6 @@ import 'package:auto_rula/auto_rula.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:vector_math/vector_math.dart';
 
-/// Key angles in a pose.
-enum KeyAngles {
-  /// The flexion angle of the left shoulder.
-  shoulderFlexionLeft,
-
-  /// The abduction angle of the left shoulder.
-  shoulderAbductionLeft,
-
-  /// The flexion angle of the right shoulder.
-  shoulderFlexionRight,
-
-  /// The abduction angle of the right shoulder.
-  shoulderAbductionRight,
-
-  /// The flexion angle of the left elbow.
-  elbowFlexionLeft,
-
-  /// The flexion angle of the right elbow.
-  elbowFlexionRight,
-
-  /// The flexion angle of the left wrist.
-  wristFlexionLeft,
-
-  /// The flexion angle of the right wrist.
-  wristFlexionRight,
-
-  /// The flexion angle of the left upper leg.
-  legFlexionLeft,
-
-  /// The flexion angle of the right upper leg.
-  legFlexionRight,
-
-  /// The flexion angle of the left knee.
-  kneeFlexionLeft,
-
-  /// The flexion angle of the right knee.
-  kneeFlexionRight,
-
-  /// The forward bend angle of the trunk.
-  trunkStoop,
-
-  /// The twist angle of the trunk.
-  trunkTwist,
-
-  /// The lateral flexion angle of the trunk.
-  trunkSideBend,
-
-  /// The forward flexion angle of the neck.
-  neckFlexion,
-
-  /// The lateral flexion angle of the neck.
-  neckSideBend,
-
-  /// The twist angle of the neck.
-  neckTwist
-}
-
 /// Mapping of all angles in a pose. Angles are in degrees.
 typedef PoseAngles = IMap<KeyAngles, double>;
 
