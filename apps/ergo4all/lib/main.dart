ff5ab9d594e606c2efbb1389746f3b6b91af7208
Ramon Brullo <ramon.brullo@proton.me>
2025-11-28 11:01:40 +0100

    fix: incorrect google font import
    Google fonts should be imported in a different way than was done until now. It now follows the instructions from their readme: https://github.com/flutter/packages/tree/main/packages/google_fonts

--- apps/ergo4all/lib/main.dart
+++ apps/ergo4all/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:ergo4all/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Add font licenses
+  LicenseRegistry.addLicense(() async* {
+    final montserrat = await rootBundle.loadString('fonts/montserrat/OFL.txt');
+    yield LicenseEntryWithLineBreaks(<String>['google_fonts'], montserrat);
+
+    final nunito = await rootBundle.loadString('fonts/nunito/OFL.txt');
+    yield LicenseEntryWithLineBreaks(<String>['google_fonts'], nunito);
+  });
 
   runApp(const Ergo4AllApp());
 }
